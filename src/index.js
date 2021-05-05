29e96e288b75bd8a3a33e249e7e89f2653d30628
Dmytro Sagaidachnyi <lafuma4me@gmail.com>
2021-05-06 01:20:09 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,42 +1,91 @@
 import './css/styles.css';
-
-import imageCardTpl from './templates/imageCard.hbs';
-import API from './js/apiService.js';
+import imageListTpl from './templates/imageList.hbs';
+import HitsApiService from './js/Api/apiService';
 
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+var debounce = require('lodash.debounce');
+const hitsApiService = new HitsApiService();
 
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    galleryList: document.querySelector('.gallery'),
+    input: document.querySelector('.search-input'),
+    sentinel: document.querySelector('#sentinel'),
+};
 
-import "@pnotify/core/dist/PNotify.css";
-import "@pnotify/core/dist/BrightTheme.css";
-const { error, success } = require('@pnotify/core');
+refs.input.addEventListener('input', debounce(onSearch, 500));
 
-const debounce = require('lodash.debounce');
+function onSearch(e) {
+    e.preventDefault();
 
+    hitsApiService.query = e.target.value.trim();
+    if(hitsApiService.query.length === 0) {
+        
+        return clearHitsContainer();
+    } 
 
-
-const searchForm = document.querySelector('.search-form');
-const galleryList = document.querySelector('.gallery');
-const input = document.querySelector('.search-input');
-const sentinel = document.querySelector('#sentinel');
-
-input.addEventListener('input', debounce(onSearch, 500))
-
-function addMarUp(data) {
-    galleryList.insertAdjacentHTML('beforeend', imageCardTpl(data));
+    hitsApiService.resetPage();
+    clearHitsContainer();
+    onLoadMore(); 
 }
-function onSearch(e){
-    console.log('hu');
+
+function onLoadMore() {
+   hitsApiService.fetchHits().then(galleryRequests);
 }
 
+function galleryRequests(data) {
+    console.log(data.hits);
+           
+        if(hitsApiService.renderedImg < data.totalHits) {
+            observer.observe(refs.sentinel);
+            addHitsMarkup(data.hits);
+            hitsApiService.renderedImg += 12;
+        }
+
+        if(hitsApiService.renderedImg === data.totalHits) {
+            observer.unobserve(refs.sentinel);
+            return;
+        }
+
+        if(hitsApiService.renderedImg + 12 > data.totalHits) {
+            observer.unobserve(refs.sentinel);
+        }
+    }
+
+function addHitsMarkup(data) {
+    refs.galleryList.insertAdjacentHTML('beforeend', imageListTpl(data));
+}
+
+function clearHitsContainer() {
+ refs.galleryList.innerHTML = '';    
+}
+
+const observer = new IntersectionObserver(onEntry, options);
+
+
+    function onEntry (entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting && hitsApiService.query !== '') {
+                console.log("LOADING...");
+                hitsApiService.fetchHits().then(galleryRequests);
+            }
+        });
+    }
+    const options = {
+        rootMargin: '100px'
+    }   
+
+//модалка basicLightbox
+refs.galleryList.addEventListener('click', onGalleryElClick);    
 
 function onGalleryElClick(event) {
-    if (event.target.nodeName !== 'IMG') {
-      return;
-    }
-    
-    const changeModalImage = `<img src=${event.target.dataset.source} alt="icon" />`;
-    const instance = basicLightbox.create(changeModalImage);
-    instance.show();
-}  
+        if (event.target.nodeName !== 'IMG') {
+          return;
+        }
+        
+        const changeModalImage = `<img src=${event.target.dataset.source} alt="icon" />`;
+        const instance = basicLightbox.create(changeModalImage);
+        instance.show();
+ }
---
+++ src/js/Api/apiService.js
@@ -0,0 +1,40 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '19367705-6c391461160466e2850694aab';
+
+export default class apiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.renderedImg = 0;
+    }
+
+    fetchHits() {
+        const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&per_page=12&page=${this.page}`;
+
+        return fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                this.incrementPage();
+
+                return data;
+            });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+        this.renderedImg = 0;
+    }
+
+    // Если убрать get и set, тогда везьде обращаемся только к searchQuery, a не query 
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
